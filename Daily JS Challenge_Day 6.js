da7961f63d251fd610d09ac4e92f776a56315714
Dearborn42 <amurf26@outlook.com>
2023-08-17 15:17:22 -0700

    adding homework

---
+++ Daily JS Challenge_Day 6.js
@@ -0,0 +1,107 @@
+const fs = require('fs');
+
+try{
+    const passwordList = fs.readFileSync('Daily JS Challenge_Day 5_Input.txt', 'utf8').split('\n').map(line => line.replace('\r', '')).map(line => {
+        const parts = line.split(":");
+        const password = parts[1].trim().replace(/\s+/g, '');
+        return [parts[0], password]
+    })
+    let totalValid = 0;
+     passwordList.filter(x => {
+        const [policy, password] = x;
+        const [letter] = policy.split(" ")[1];
+        const [min, max] = policy.split(" ")[0].split("-").map(x => parseInt(x, 10));
+        if((password.charAt(min-1) === letter && password.charAt(max-1) != letter)||(password.charAt(min-1) != letter && password.charAt(max-1) === letter)){
+            totalValid++;
+        }
+     });
+     console.log(totalValid);
+}
+catch{
+   
+}
+
+/* NEW JS CHALLENGE Part 1 *******
+
+With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
+
+Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
+
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
+
+..##.........##.........##.........##.........##.........##.......  --->
+#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........#.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...##....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
+
+The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
+
+From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
+
+The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
+
+..##.........##.........##.........##.........##.........##.......  --->
+#..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........X.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...#X....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+In this example, traversing the map using this slope would cause you to encounter 7 trees.
+
+Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
+ */
+
+
+try{
+    const map = fs.readFileSync('Daily JS Challenge_Day 6_Input.txt', 'utf8').split('\r\n')
+    let x = 0;
+    let y = 0;
+    let totalTrees = 0;
+
+    while (y < map.length) {
+        console.log(x)
+        if (map[y].charAt(x) === "#") {
+            totalTrees++;
+        }
+        
+        const right = 3;
+        if (x + right < map[y].length) {
+            x += right;
+        } else {
+            const diff = x + right - map[y].length;
+            x = diff >= 0 ? diff : 0;
+        }
+        
+        y++;
+    }
+
+    console.log("Total trees encountered:", totalTrees);
+
+}
+catch{
+
+}
