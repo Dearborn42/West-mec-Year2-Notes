00b394cede833e3beab80670220254f1159fa3d3
Dearborn42 <amurf26@outlook.com>
2023-08-23 15:20:36 -0700

    working on server

--- NodeJS/01-node-tutorial/14-request-events.js
+++ NodeJS/01-node-tutorial/14-request-events.js
@@ -1,14 +1,28 @@
 const http = require('http');
+const emmitter = require('events');
 
+const customEmmitter = new emmitter();
+
+let siteVisited = 0;
 const server = http.createServer();
 
 server.on("request", (req, res) => {
     if(req.url === '/about'){
         console.log(req.url);
-        req.readableEnded("About");
-    }else{
-        res.end("Welcome")
+        customEmmitter.emit('visited', req.method, req.url)
+        req.end("about");
+    }else if(req.url === '/time'){
+        res.sendFile(__dirname + '/aboutUs.html')
     }
 })
 
-server.listen(3000);
+
+customEmmitter.on('visited', (method, url) => {
+    siteVisited++;
+    console.log(`Event has been triggered: This site has been visited`);
+    console.log(`Method is: ${method}, url is: ${url}`);
+})
+
+server.listen(4000, () => {
+    console.log(`Server listening on 3000`);
+});
