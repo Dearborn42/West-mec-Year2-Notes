77514315cf724b55e3c3d7627503950a52102f40
Dearborn42 <amurf26@outlook.com>
2023-08-28 15:16:23 -0700

    notes

--- NodeJS/01-node-tutorial/16-streams.js
+++ NodeJS/01-node-tutorial/16-streams.js
@@ -5,3 +5,12 @@ const path = require('path');
 
 
 const stream = createReadStream(path.join(__dirname, '/content/bigFile.txt'), {encoding: 'utf8'});
+
+// there are 4 fundamental streams types in node.js
+// readable, writeable, duplex, and transform steams
+
+
+// readable stream is an abstraction for a source from which data can be consumed
+// writeable stream is an abstraction for a source from which data can be written
+// duplex stream is both readable and writable
+// transform stream is basically a duplex that can be used to modify or transform data
---
+++ NodeJS/01-node-tutorial/17-http-stream.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+http.createServer((req, res)=>{
+    const text = fs.readFileSync(path.join(__dirname, './content/bigFile.txt'), 'utf8');
+    res.end();
+}).listen(3000, ()=>{
+    console.log('server listening on port 3000');
+})
