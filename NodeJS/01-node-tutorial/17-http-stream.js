a934107a3dc32f074b92eb9661cb1abb4fac6c56
Dearborn42 <amurf26@outlook.com>
2023-08-29 15:14:32 -0700

    making an express server

--- NodeJS/01-node-tutorial/17-http-stream.js
+++ NodeJS/01-node-tutorial/17-http-stream.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 http.createServer((req, res)=>{
     const text = fs.readFileSync(path.join(__dirname, './content/bigFile.txt'), 'utf8');
-    res.end();
+    res.end(text);
 }).listen(3000, ()=>{
     console.log('server listening on port 3000');
 })
