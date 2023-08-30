369001f5bb98529914889103a17d4b255a450451
Dearborn42 <amurf26@outlook.com>
2023-08-30 13:27:51 -0700

    notes

--- NodeJS/02-express/app.js
+++ NodeJS/02-express/app.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+// Set up static middleware
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    console.log(req.url);
+    res.sendFile(path.join(__dirname, '/public'));
+})
+
+
+app.get('*', (req, res) => {
+    res.status(404).sendFile("404 not found");
+})
+
+app.listen(5000, () =>{
+    console.log("listening on port 5000")
+})
