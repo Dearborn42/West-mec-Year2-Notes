bba31ebdf1bf6a6b6107e99d3331630c7b10bf64
Dearborn42 <amurf26@outlook.com>
2023-09-07 15:16:14 -0700

    notes

--- NodeJS/02-express/06-params-juqery.js
+++ NodeJS/02-express/06-params-juqery.js
@@ -4,6 +4,7 @@ const {products} = require('./data.js');
 
 app.get('/', (req, res) => {
     res.send('<h1>This is the home page</h1><a href="/api/products">Products</a>');
+    res.end();
 })
 
 
@@ -13,8 +14,37 @@ app.get('/api/products', (req, res)=>{
         return {id, name, image, price, desc}
     })
 
-    res.send(newProducts);
+    res.json(newProducts);
     res.end();
+});
+
+app.get('/api/products/:productId', (req, res) => {
+    console.log(req.params);
+    const { productId } = req.params
+    const singleProduct = products.find(x => x.id === Number(productId));
+    if(!singleProduct)
+        return res.status(404).send("Product not found")
+    else
+        return res.json(singleProduct)
+});
+
+
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query);
+    const { search, limit } = req.query
+    let sortedProducts = [...products];
+    if(search){
+        sortedProducts = sortedProducts.filter((x) => {
+            return x.name.startsWith(search)
+        })
+    }
+    if(limit)
+        sortedProducts = sortedProducts.slice(0, Number(limit));
+    
+    if(sortedProducts.length < 2)
+        return res.status(200).json({success: true, data: []})
+
+    res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, ()=>{
--- NodeJS/02-express/app.js
+++ NodeJS/02-express/app.js
@@ -18,5 +18,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(5000, () =>{
-    console.log("listening on port 5000")
+    console.log("listening on port 3000")
 })
